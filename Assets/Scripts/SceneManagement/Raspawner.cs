9b78beaf358264052ab3a1a216b24caebbdcb45d
Ronald <foxmulderven@gmail.com>
2019-08-04 12:38:09 -0600

    respwner and bg added

---
+++ Assets/Scripts/SceneManagement/Raspawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raspawner : MonoBehaviour
+{
+    public Vector3 startingPosition;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If player touches this, respawn it to starting pos
+        collision.transform.position = startingPosition;
+    }
+
+
+}
