dfb84d7a73547970e04efdbeaae368c80a43c98b
Noah Sragow <noah@Noahs-Air.fios-router.home>
2019-08-04 13:28:27 -0400

    menu`

---
+++ Assets/Scenes/Menu/timeloadnex.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timeloadnex : MonoBehaviour
+{
+    public float timer = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
